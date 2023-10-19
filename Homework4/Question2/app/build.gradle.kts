fa030f02804e6e877f2bd1611fa7a63cdbeb2d60
Sicheng Chen <csc0007@outlook.com>
2023-10-19 11:52:29 -0400

    deleted old files

--- Homework4/Question2/app/build.gradle.kts
+++ Homework4/Question2/app/build.gradle.kts
@@ -4,17 +4,20 @@ plugins {
 }
 
 android {
+    namespace = "com.bignerdranch.android.sensor"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.bignerdranch.android.sensorapp"
-        minSdk = 24
-        targetSdk = 34
+        applicationId = "com.bignerdranch.android.sensor"
+        minSdk = 30
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables.useSupportLibrary = true
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -26,22 +29,28 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
-    kotlinOptions.jvmTarget = "1.8"
-
-    buildFeatures.compose = true
-
-    composeOptions.kotlinCompilerExtensionVersion = "1.4.3"
-
-    packaging.resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
@@ -51,12 +60,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.6.1")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
     debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
