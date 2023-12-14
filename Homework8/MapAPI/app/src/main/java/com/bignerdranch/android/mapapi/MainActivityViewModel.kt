18bbceee8d2b4692d2465ca44c6066e94add9c11
siyi756 <1481929028@qq.com>
2023-12-14 14:47:16 -0500

    Update MapAPI
    Update MapAPI

--- Homework8/MapAPI/app/src/main/java/com/bignerdranch/android/mapapi/MainActivityViewModel.kt
+++ Homework8/MapAPI/app/src/main/java/com/bignerdranch/android/mapapi/MainActivityViewModel.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.mapapi
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,6 +10,8 @@ import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
+import java.net.SocketTimeoutException
+import java.util.concurrent.TimeUnit
 
 class MainActivityViewModel : ViewModel() {
 
@@ -23,20 +26,47 @@ class MainActivityViewModel : ViewModel() {
                 val request = Request.Builder()
                     .url("https://driving-directions1.p.rapidapi.com/get-directions?origin=$urlOrigin&destination=$urlDestination&avoid_routes=tolls,ferries&country=us&language=en")
                     .get()
-                    .addHeader("X-RapidAPI-Key", "YOUR-API-KEY")
+                    .addHeader("X-RapidAPI-Key", "e0fb2d06ebmsh13b4199c11aa6c3p1d57a3jsn2277e5c7358d")
                     .addHeader("X-RapidAPI-Host", "driving-directions1.p.rapidapi.com")
                     .build()
-
+                val client = OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .build()
                 val response = client.newCall(request).execute()
-                val jsonData = JSONObject(response.body?.string() ?: "")
-                val route = jsonData.getJSONObject("data").getJSONArray("best_routes").getJSONObject(0)
-                val distance = route.getString("distance_label")
-                val duration = route.getString("duration_label")
+                /*// Log the response for debugging
+                val responseBody = response.body?.string() ?: ""
+                Log.d("API Response", responseBody)*/
+                val jsonData = JSONObject(response.body?.string())
+                // Check if the "data" field is present in the JSON response
+                if (jsonData.getString("status") == "OK" && jsonData.has("data")) {
+                    val data = jsonData.getJSONObject("data")
 
-                withContext(Dispatchers.Main) {
-                    routeInfo.value = "Distance: $distance\nDuration: $duration"
+                    if (data.has("best_routes")) {
+                        val bestRoutes = data.getJSONArray("best_routes")
+                        val firstRoute = bestRoutes.getJSONObject(0)
+
+                        // Extracting some basic information
+                        val routeName = firstRoute.getString("route_name")
+                        val distance = firstRoute.getString("distance_label")
+                        val duration = firstRoute.getString("duration_label")
+
+                        withContext(Dispatchers.Main) {
+                            routeInfo.value = "Route: $routeName, Distance: $distance, Duration: $duration"
+                        }
+                    }
+                } else {
+                    // Handle the case where "data" field is not present
+                    withContext(Dispatchers.Main) {
+                        routeInfo.value = "No route information available"
+                    }
                 }
-            } catch (e: Exception) {
+            } catch (e: SocketTimeoutException) {
+                withContext(Dispatchers.Main) {
+                    routeInfo.value = "Request timed out. Please try again."
+                }
+            }catch (e: Exception) {
                 withContext(Dispatchers.Main) {
                     routeInfo.value = "Error: ${e.message}"
                 }
