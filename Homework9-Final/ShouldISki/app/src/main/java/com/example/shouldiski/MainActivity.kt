03cf9bef0656e2e8d7d9ae62953a3571b8858461
siyi756 <1481929028@qq.com>
2023-12-20 01:58:04 -0500

    Hide top and bottom bar

--- Homework9-Final/ShouldISki/app/src/main/java/com/example/shouldiski/MainActivity.kt
+++ Homework9-Final/ShouldISki/app/src/main/java/com/example/shouldiski/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.shouldiski
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -26,6 +27,12 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.welcomeFragment -> binding.navView.visibility = View.GONE
+                else -> binding.navView.visibility = View.VISIBLE
+            }
+        }
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
--- Homework9-Final/ShouldISki/app/src/main/java/com/example/shouldiski/ui/welcome/WelcomeFragment.kt
+++ Homework9-Final/ShouldISki/app/src/main/java/com/example/shouldiski/ui/welcome/WelcomeFragment.kt
@@ -5,9 +5,11 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.shouldiski.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class WelcomeFragment : Fragment() {
 
@@ -20,8 +22,16 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Hide the action bar and bottom navigation bar
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+        activity?.findViewById<BottomNavigationView>(R.id.nav_view)?.visibility = View.GONE
+
         Handler().postDelayed({
             findNavController().navigate(R.id.action_welcomeFragment_to_profileFragment)
+            // Restore the action bar and bottom navigation bar when navigating away
+            (activity as? AppCompatActivity)?.supportActionBar?.show()
+            activity?.findViewById<BottomNavigationView>(R.id.nav_view)?.visibility = View.VISIBLE
         }, 5000)
     }
 }
